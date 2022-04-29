86d4f4233cfd4205ccff368278d94c422afe55ef
Felipe Sergio <felipesilva@MacBook-Pro-de-Felipe.local>
2022-04-29 20:48:21 -0300

    add swagger in project

--- src/app.js
+++ src/app.js
@@ -3,13 +3,17 @@ require("dotenv-safe").config()
 const express = require('express')
 const routes = require('./routes')
 const cors = require('cors')
+
+const swaggerUi = require('swagger-ui-express')
+const swaggerFile = require('../swagger_output.json')
+
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
 app.use(routes)
-
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 app.use((error,req,res,next)=>{
     res.status(error.status || 500)
     res.json({error: error.menssage})
---
+++ swagger.js
@@ -0,0 +1,7 @@
+const swaggerAutogen = require('swagger-autogen')()
+
+const outputFile = './swagger_output.json'
+const endpointsFiles = ['./src/routes']
+
+swaggerAutogen(outputFile, endpointsFiles)
+
